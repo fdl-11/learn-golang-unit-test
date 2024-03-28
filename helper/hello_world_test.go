f3ab1c9d6afc60df1ccc459be11a89f1c4796cb6
fdl-11 <fadlidarusalam11@gmail.com>
2024-03-28 13:08:43 +0700

    upd test main & skip test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -2,11 +2,34 @@ package helper
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+// Jika terdapat function TestMain, maka secara otomatis Go-Lang akan mengeksekusi function ini tiap kali akan menjalankan unit test di sebuah package
+// TestMain dieksekusi hanya sekali per Go-Lang package, bukan per tiap function unit test
+func TestMain(m *testing.M) {
+	// Before Testing
+	fmt.Println("Before Unit Test")
+
+	m.Run()
+
+	// After Testing
+	fmt.Println("After Unit Test")
+}
+
+// Jika OS nya adalah Mac OS, maka test-nya di skip / dilewati
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "darwin" { // GO Operation System mac os = darwin
+		t.Skip("Can not run on Mac OS")
+	}
+
+	result := HelloWorld("Fadli")
+	require.Equal(t, "Hello Fadli", result, "Result should be Hello Fadli")
+}
 
 // Require menggunakan failNow() / jika error maka kode tidak dilanjutkan
 func TestHelloWorldRequire(t *testing.T) {
